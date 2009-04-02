58ab803dd16450203fac3aa1ec1e7a943ca25657
Mike Stangel <mike@geni.com>
2009-04-02 16:24:01 -0700

    Implementation of batch and batch_event, started observer_batch_event

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,12 @@ require 'test/unit'
 require 'shoulda'
 require 'mocha'
 
+$LOAD_PATH.unshift("#{File.dirname(__FILE__)}/../lib")
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'pg_queue'
+require 'pg_queue/batch'
+require 'pg_queue/batch_event'
+require 'pg_queue/observer_batch_event'
 
 class Test::Unit::TestCase
 end
